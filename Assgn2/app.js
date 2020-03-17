5f3e911489360387a302825e06208889f751f5e7
Nabeel07925 <muhammad.nabeel38@yahoo.com>
2020-03-17 10:58:10 +0500

    first commit

---
+++ Assgn2/app.js
@@ -0,0 +1,11 @@
+new Vue({
+        el: '#exercise',
+        data: {
+            value: 'fdddddddddd'
+        },
+    methods: {
+            showAlert() {
+                alert('This is a general alert')
+            }
+    }
+    });
